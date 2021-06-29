5e45d203d8f4e5da7c2e3ce04e258753e9375e82
krism <krism@krisms-MBP.modem>
2021-06-29 15:21:23 +1000

    extra tests for cubist

--- tests/testthat/test-check_original_vkVal.R
+++ tests/testthat/test-check_original_vkVal.R
@@ -6,10 +6,30 @@ testData <- function(strobjrun,strobktest) {
   identical(objThatRan,objTest)
 }
 
-test_that("Main RData files generated as output unchanged", {
+test_that("Main RData files generated as output unchanged - Spline Model", {
   
   expect_equal(2 * 2, 4)
   expect_equal(testData(here::here("tests/original_results/vkVal.rds"),here::here("tests/run_results/vkVal.rds")),TRUE)
   expect_equal(testData(here::here("tests/original_results/zkVal.rds"),here::here("tests/run_results/zkVal.rds")),TRUE)
   expect_equal(testData(here::here("tests/original_results/lmm.fit.selected.rds"),here::here("tests/run_results/lmm.fit.selected.rds")),TRUE)
 })
+
+test_that("Main RData files generated as output unchanged Cubist Model", {
+  expect_equal(testData(here::here("tests/original_results_cubist/vkVal.rds"),here::here("tests/run_results/vkVal.rds")),TRUE)
+  expect_equal(testData(here::here("tests/original_results_cubist/zkVal.rds"),here::here("tests/run_results/zkVal.rds")),TRUE)
+  expect_equal(testData(here::here("tests/original_results_cubist/lmm.fit.selected.rds"),here::here("tests/run_results/lmm.fit.selected.rds")),TRUE)
+})
+
+test_that("zkVal Cubist Model", {
+  expect_equal(testData(here::here("tests/original_results_cubist/zkVal.rds"),here::here("tests/run_results/zkVal.rds")),TRUE)
+})
+
+test_that("vkVal Cubist Model", {
+  expect_equal(testData(here::here("tests/original_results_cubist/vkVal.rds"),here::here("tests/run_results/vkVal.rds")),TRUE)
+})
+
+test_that("lmm.fit.selected.rdsCubist Model", {
+  expect_equal(testData(here::here("tests/original_results_cubist/lmm.fit.selected.rds"),here::here("tests/run_results/lmm.fit.selected.rds")),TRUE)
+})
+
+
